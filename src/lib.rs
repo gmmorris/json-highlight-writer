3b7da8b78eb1f2abcc06955ae164dd3b4f2c170e
chekofif <gidi@gidi.io>
2019-04-16 20:20:17 +0100

    chore(generator): Added basic test over DumpGenerator just to make sure we don't accidentaly break some basic logic in the untested generator we borrowed

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,29 @@ pub fn dump(json_object: Object) -> String {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use json::*;
+    
     #[test]
-    fn it_works() {
-        super::main();
+    fn dump_should_work_as_expected() {
+        let data = object!{
+            "foo" => false,
+            "bar" => json::Null,
+            "answer" => 42,
+            "list" => array![json::Null, "world", true],
+            "obj" => object!{
+                "foo" => false,
+                "bar" => json::Null,
+                "answer" => 42,
+                "list" => array![json::Null, "world", true]
+            }
+        };
+
+        let expected_json = r#"{"foo":false,"bar":null,"answer":42,"list":[null,"world",true],"obj":{"foo":false,"bar":null,"answer":42,"list":[null,"world",true]}}"#;
+
+        match data {
+            JsonValue::Object(obj) => assert_eq!(dump(obj),expected_json),
+            _ => panic!("Invalid result")
+        }
     }
 }
